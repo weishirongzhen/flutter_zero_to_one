8b9dafe9c6a330628644aa5037264f8d386635c5
memtopia <memtopia@gmail.com>
2019-10-02 17:33:19 +0800

    添加circleci 配置

---
+++ lib/http/api_address.dart
@@ -0,0 +1,20 @@
+class ApiAddress {
+
+  ///黑白图像上色
+  static const String colourize = 'https://aip.baidubce.com/rest/2.0/image-process/v1/colourize';
+
+  ///图像无损放大
+  static const String image_quality_enhance = 'https://aip.baidubce.com/rest/2.0/image-process/v1/image_quality_enhance';
+
+  ///图像去雾
+  static const String dehaze = 'https://aip.baidubce.com/rest/2.0/image-process/v1/dehaze';
+
+  ///图像对比度增强
+  static const String contrast_enhance = 'https://aip.baidubce.com/rest/2.0/image-process/v1/contrast_enhance';
+
+  ///拉伸图像恢复
+  static const String stretch_restore = 'https://aip.baidubce.com/rest/2.0/image-process/v1/stretch_restore';
+
+  ///公众人物识别
+  static const String img_censor = 'https://aip.baidubce.com/api/v1/solution/direct/img_censor';
+}
---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('aaa'),
+      ),
+      body: Text(''),
+    );
+  }
+}
