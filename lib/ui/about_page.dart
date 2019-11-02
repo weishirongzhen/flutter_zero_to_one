d6deebab85e18c6b4220b206d122d4d8ce1d8969
memtopia <memtopia@gmail.com>
2019-11-02 23:26:57 +0800

    完善关于界面

--- lib/ui/about_page.dart
+++ lib/ui/about_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:package_info/package_info.dart';
 
 class AboutPage extends StatelessWidget {
   @override
@@ -8,7 +9,58 @@ class AboutPage extends StatelessWidget {
         centerTitle: true,
         title: Text('关于'),
       ),
-      body: Container(),
+      body: Container(
+        width: double.infinity,
+        child: SingleChildScrollView(
+          child: FutureBuilder<PackageInfo>(
+              future: PackageInfo.fromPlatform(),
+              builder: (context, snapshot) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Center(
+                      child: Container(
+                        width: 200,
+                        child: Image.asset('assets/images/app.webp'),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Center(
+                      child: Text(
+                        '${snapshot.hasData ? snapshot.data.appName : ''}',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Center(
+                      child: Text(
+                        'Version ${snapshot.hasData ? snapshot.data.version : ''}',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(14.0),
+                      child: Text(
+                        '''这个App是我的公众号【码托邦】的 \"Flutter从零到上架应用商店\"系列文章的示例App，更多介绍请关注我的微信公众号。''',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              }),
+        ),
+      ),
     );
   }
 }
